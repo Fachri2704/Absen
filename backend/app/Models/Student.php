7ab544d507b1f4476630910e2528a0ca646ef843
Fachrii <fachriafrian043@gmail.com>
2026-05-11 20:18:56 +0700

    adding table

---
+++ backend/app/Models/Student.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    protected $table = 'classes';
+
+    protected $fillable = [
+        'name',
+        'level',
+        'major',
+    ];
+
+    public function students(){
+        return $this->hasMany(Student::class, 'class_id');
+    }
+}
